40e121e081c658b9c391298a6e23e6050fed505d
Ismael Moral <jastertdc@gmail.com>
2023-08-20 19:36:46 +0200

    refactor(MT-XXXX): add get box price history in box price repository

--- src/BoxPrice/Domain/Repository/BoxPriceRepository.php
+++ src/BoxPrice/Domain/Repository/BoxPriceRepository.php
@@ -3,8 +3,11 @@
 namespace JasterTDC\PriceHistory\BoxPrice\Domain\Repository;
 
 use JasterTDC\PriceHistory\BoxPrice\Domain\BoxPrice;
+use JasterTDC\PriceHistory\Shared\Domain\BoxId;
 
 interface BoxPriceRepository
 {
+    /** @return array<int, BoxPrice> */
+    public function getBoxPriceHistory(BoxId $boxId): array;
     public function save(BoxPrice ...$boxPrices): void;
 }
