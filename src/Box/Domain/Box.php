2320b427dbdd95d2ac5d060b9407681ba3667a40
Ismael Moral <jastertdc@gmail.com>
2023-08-27 13:43:32 +0200

    feat(MT-XXXX): add category id in box entity

--- src/Box/Domain/Box.php
+++ src/Box/Domain/Box.php
@@ -4,12 +4,14 @@ namespace JasterTDC\PriceHistory\Box\Domain;
 
 use JasterTDC\PriceHistory\Shared\Domain\Name;
 use JasterTDC\PriceHistory\Shared\Domain\BoxId;
+use JasterTDC\PriceHistory\Shared\Domain\CategoryId;
 use JasterTDC\PriceHistory\Shared\Domain\Rating;
 
 final readonly class Box
 {
     public function __construct(
         private BoxId $id,
+        private CategoryId $categoryId,
         private Name $name,
         private Rating $rating
     ) {
@@ -18,6 +20,11 @@ final readonly class Box
     public function id(): int
     {
         return $this->id->value();
+    }
+
+    public function categoryId(): int
+    {
+        return $this->categoryId->value();
     }
 
     public function name(): string
--- src/Box/Domain/Factory/BoxFactory.php
+++ src/Box/Domain/Factory/BoxFactory.php
@@ -4,27 +4,27 @@ namespace JasterTDC\PriceHistory\Box\Domain\Factory;
 
 use JasterTDC\PriceHistory\Box\Domain\Box;
 use JasterTDC\PriceHistory\Shared\Domain\BoxId;
+use JasterTDC\PriceHistory\Shared\Domain\CategoryId;
 use JasterTDC\PriceHistory\Shared\Domain\Name;
-use JasterTDC\PriceHistory\Shared\Domain\Price;
 use JasterTDC\PriceHistory\Shared\Domain\Rating;
 
 final class BoxFactory
 {
     public static function build(
         int $primitiveBoxId,
+        int $primitiveCategoryId,
         string $primitiveName,
-        float $primitivePrice,
         float $primitiveRating
     ): Box {
         $boxId = new BoxId($primitiveBoxId);
+        $categoryId = new CategoryId($primitiveCategoryId);
         $name = new Name($primitiveName);
-        $price = new Price($primitivePrice);
         $rating = new Rating($primitiveRating);
 
         return new Box(
             id: $boxId,
+            categoryId: $categoryId,
             name: $name,
-            price: $price,
             rating: $rating
         );
     }
